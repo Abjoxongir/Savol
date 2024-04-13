8895779af81083565bbbecf96dbee66d8fca4946
Abjoxongir <166017030+Abjoxongir@users.noreply.github.com>
2024-04-13 18:06:07 +0500

    switch орагниб чиктим

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-let Username = prompt("User name wot  due du")
-let Poswords = Number(prompt("User posword wot due du"))
-let age = Number(prompt("User age wot due du"))
-
-if (Username === "Joxa" && Poswords <=1000 && age <=10 ){
-    alert("Joxa Joxa Чо том Чо там как там")
-    confirm ('это правда Joxa joxa ?')
+let Name = prompt("Name user")
+switch(Name){
+    case Name = "Joxongir": alert("4o tam joxa"); break;
     
-}else if (Username === "Zafar"&& (Poswords <= 10000 || age <=20)){
-    alert("Zapulo Zapush чо там чо там Нигер")
-    confirm("Zapuch Papush тошнами бу йо тошнамасми бу")
-}else if (Username === "Sevara" && (Poswords <= 10000 || age <=5)){
-    alert("Sevara chevara sattoriva lataeva sembabu aaaa")
-    confirm("Sevara lataeva toshna senba bu yo senbasmabu")
-}else if (Username === "Abdujalil"&& (Poswords<= 100000 || age <= 15) ){
-alert("Abujalil senba bu eee ")
-confirm("Ebe satorali ma bu")
-}else {
-    alert("KIMSAN SAN OOOOOO")
-    confirm("IT BSEN SENOOO")
-    confirm("YO ESHAK BSEN SENOOO")
-    confirm("TOSHNABA")
+    case Name = "zafar":
+    case Name = "sevara":
+    case Name = "abu":alert ("Meng Joxa Toxa kerak Chaqirooo"); break;
+
+    default: alert("kimsan sn ooo")
 }
