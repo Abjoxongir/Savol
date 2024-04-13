3013feab0932bbe4585e512f0d1d3a17ec019696
Abjoxongir <166017030+Abjoxongir@users.noreply.github.com>
2024-04-13 16:01:55 +0500

    kara kara bu  qavslar bilan ishlash

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-let name1 = prompt("User name")
-let surname = prompt("surname")
-let age = prompt ("Age")
-
-if (name1 === "admin" || surname === "user" && age <= 10 ){
-    alert("hush kelibsiz")
-    document.write(`<img src="собака.jpg"/>`)
-}else if (name1 === "admin" || surname === "user" && age <= 20 ){
-    alert("hush kelibsiz")
-    document.write(`<img src="гф.gif"/>`)
-
-}else if (name1 === "joxa" && surname === "toxa" && age === "25"){
-    confirm("Vay oooo Joxa toxa mi bu ")
-    alert("toshnamu bu")
-    confirm("Anqpa bu")
-    alert("Mujiqo buu")
-    confirm("Yo mayo")
-    document.write(`<img src="гф.gif"/>`)
-}
-
-
-else{
-    alert("Кто ты ее по жизне ?")
+let i = Number(prompt("2*2=?"))
+let i2 = Number(prompt("4*4=?"))
+let i3 = Number(prompt("5*8=?"))
+let i4 = Number(prompt("7*6=?"))
+let i5 = Number(prompt("5*3=?"))
+let i6 = Number(prompt("5*2=?"))
+if ((i === 4 && i2 === 16 && i3 === 40) || (i4 === 42 && i5 === 15 && i6 === 10)){
+    alert("Yaxshi ooo bola")
+document.write(`<img src="гф.gif"/>`)
+}else {
+    alert ("uchi zanova tablisu")
 }
