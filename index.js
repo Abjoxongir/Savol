ac238c779cfc413dd36573f6eea73d06e27892db
Abjoxongir <166017030+Abjoxongir@users.noreply.github.com>
2024-04-13 09:16:56 +0500

    if else ишлаш 1 мисол

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-let a = 10 +'ad'+1n 
-let b = "10" // string
-let c = true // boolean
-let q = null // null
-let r = undefined // undefined
-let s = 1n // Biginteger
-let t = Symbol()// saymbol
-let u = {}
-let v = []
-let a2 = Number(a) // Namber
-console.log (typeof a2)
+let a = "Mol"
+let a1 = "musuk"
+let a2 = "it"
+let a3 = "ot"
+let a4 = "baliq"
+let a5 = "touq"
+let a6 = "sigir"
+
+let b =  "  ot  eydi"
+let b1 =  " gosht eydi"
+let b2 =  " boq eydi"
+let b3 =  " hashak eydi"
+let b4 =  " chualchang eydi"
+let b5 =  " don eydi"
+let b6 =  " ot hashak eydi"
+
+
+
+let Name = prompt("Uy hayvonini kiriting")
+if (Name === "mol"){
+    alert(a+" "+b)
+}else if (Name === "mushuk"){
+ alert(a1+" "+b1)
+}else if (Name === "it"){
+alert(a2+" "+b2)
+}else if (Name === "ot"){
+    alert(a3+" "+b3)
+}else if (Name === "baliq"){
+    alert(a4+" "+b4)
+}else if (Name === "touq"){
+    alert(a5+" "+b5)
+}else if (Name === "sigir"){
+    alert(a6+" "+b6)
+}
